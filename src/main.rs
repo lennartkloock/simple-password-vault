192cd4ca937586b7782b52add0f80cd30573bafe
Lennart Kloock <lennart.kloock@protonmail.com>
2021-11-03 23:28:41 +0100

    Update main.rs
    Remove `macro_use`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-#[macro_use] extern crate rocket;
-
-#[get("/")]
+#[rocket::get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[launch]
+#[rocket::launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
 }
