ca75926289a8ef57a69157e30764990a8236e993
Lennart Kloock <lennart.kloock@protonmail.com>
2021-11-05 21:06:10 +0100

    Add initial database support

---
+++ src/database.rs
@@ -0,0 +1,10 @@
+use sqlx::mysql;
+
+pub struct VaultDb(mysql::MySqlPool);
+
+pub async fn init(db_url: &str) -> Result<VaultDb, sqlx::Error> {
+    mysql::MySqlPoolOptions::new()
+        .connect(db_url)
+        .await
+        .map(|pool| VaultDb(pool))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
+use rocket::fairing;
+use rocket::serde;
+
+mod database;
+
+#[derive(serde::Deserialize)]
+pub struct VaultConfig {
+    db_url: String,
+}
+
 #[rocket::get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[rocket::launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", rocket::routes![index])
+#[rocket::main]
+async fn main() {
+    let rocket = rocket::build()
+        .attach(fairing::AdHoc::config::<VaultConfig>())
+        .mount("/", rocket::routes![index]);
+    match rocket.figment().extract::<VaultConfig>() {
+        Ok(config) => {
+            match database::init(&config.db_url).await {
+                Ok(db) => {
+                    rocket.manage(db).launch().await.expect("Rocket blew up at launch (⩾﹏⩽)");
+                }
+                Err(e) => log::error!("An error occurred while initializing the database: {}\n\
+                    Please make sure your MySQL/MariaDB server is responding at the specified url", e),
+            }
+        }
+        Err(e) => log::error!("An error occurred while reading the config: {}", e),
+    }
 }
