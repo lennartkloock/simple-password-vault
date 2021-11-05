02c814c65af438c2310431a5975bb4ed3416f416
Lennart Kloock <lennart.kloock@protonmail.com>
2021-11-05 21:12:50 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,20 @@ async fn main() {
         .attach(fairing::AdHoc::config::<VaultConfig>())
         .mount("/", routes::get_routes());
     match rocket.figment().extract::<VaultConfig>() {
-        Ok(config) => {
-            match database::init(&config.db_url).await {
-                Ok(db) => {
-                    rocket.manage(db).launch().await.expect("Rocket blew up at launch (⩾﹏⩽)");
-                }
-                Err(e) => log::error!("An error occurred while initializing the database: {}\n\
-                    Please make sure your MySQL/MariaDB server is responding at the specified url", e),
+        Ok(config) => match database::init(&config.db_url).await {
+            Ok(db) => {
+                rocket
+                    .manage(db)
+                    .launch()
+                    .await
+                    .expect("Rocket blew up at launch (⩾﹏⩽)");
             }
-        }
+            Err(e) => log::error!(
+                "An error occurred while initializing the database: {}\n\
+                    Please make sure your MySQL/MariaDB server is responding at the specified url",
+                e
+            ),
+        },
         Err(e) => log::error!("An error occurred while reading the config: {}", e),
     }
 }
