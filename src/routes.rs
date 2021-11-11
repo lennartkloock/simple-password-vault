d1bfab0b9d420df4009dd76bfb45cb77972f2ba0
Lennart Kloock <lennart.kloock@protonmail.com>
2021-11-11 22:40:49 +0100

    Cleanup

--- src/routes.rs
+++ src/routes.rs
@@ -9,28 +9,30 @@ struct GeneralContext {
     name: String,
 }
 
+impl Default for GeneralContext {
+    fn default() -> Self {
+        Self {
+            name: "Password Vault".to_string(),
+        }
+    }
+}
+
+impl From<&VaultConfig> for GeneralContext {
+    fn from(config: &VaultConfig) -> Self {
+        config
+            .name
+            .clone()
+            .map(|name| GeneralContext { name })
+            .unwrap_or_default()
+    }
+}
+
 #[rocket::get("/login")]
 fn login(config: &rocket::State<VaultConfig>) -> rocket_dyn_templates::Template {
-    rocket_dyn_templates::Template::render(
-        "login",
-        GeneralContext {
-            name: config
-                .name
-                .clone()
-                .unwrap_or(String::from("Password Vault")),
-        },
-    )
+    rocket_dyn_templates::Template::render("login", GeneralContext::from(config.inner()))
 }
 
 #[rocket::get("/vault")]
 fn vault(config: &rocket::State<VaultConfig>) -> rocket_dyn_templates::Template {
-    rocket_dyn_templates::Template::render(
-        "vault",
-        GeneralContext {
-            name: config
-                .name
-                .clone()
-                .unwrap_or(String::from("Password Vault")),
-        },
-    )
+    rocket_dyn_templates::Template::render("vault", GeneralContext::from(config.inner()))
 }
